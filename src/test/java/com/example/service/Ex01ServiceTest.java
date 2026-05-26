bab7c02d84877154e53edc37eeb9e8af0d1bdbf5
takumi2003 <takumi20030819@gmail.com>
2026-05-26 12:26:38 +0900

    ex01完成

---
+++ src/test/java/com/example/service/Ex01ServiceTest.java
@@ -0,0 +1,32 @@
+package com.example.service;
+
+import com.example.domain.baseballTeam;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@Transactional
+class Ex01ServiceTest {
+    @Autowired
+    private Ex01Service service;
+
+    @Test
+    void findAllで全てのデータが取得されている() {
+        List<baseballTeam> teams = service.findAll();
+
+        assertEquals(6, teams.size(), "findAllの取得件数が間違っています");
+    }
+
+    @Test
+    void findByIDで特定のidのデータを取得する() {
+        baseballTeam team = service.findById(1);
+
+        assertEquals("読売ジャイアンツ", team.getTeamName(), "findByIDでチーム名が違います");
+    }
+}
